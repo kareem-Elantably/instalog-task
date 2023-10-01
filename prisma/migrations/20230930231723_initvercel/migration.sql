257316d9ded2e9090781b58a81c9447421b6a6f2
Kareem Elantably <kareem.elantably@collardeg.com>
2023-10-01 03:05:07 +0300

    migration

--- prisma/migrations/20230930231723_initvercel/migration.sql
+++ prisma/migrations/20230930231723_initvercel/migration.sql
@@ -1,13 +1,12 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
 
-  - You are about to drop the column `email` on the `user` table. All the data in the column will be lost.
-  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `email`,
-    ADD COLUMN `name` VARCHAR(191) NOT NULL;
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `EventAction` (
